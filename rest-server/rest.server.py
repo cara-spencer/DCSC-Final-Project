a5235cf037730a991fe8039bde0058f9d6a13e45
cara spencer <casp4531@colorado.edu>
2022-12-10 11:36:27 -0700

    Added Cara's cleaning functions and the retrieve survey api method to rest.server.py but it hasn't been tested yet. Added a gitignore.

--- rest-server/rest.server.py
+++ rest-server/rest.server.py
@@ -2,39 +2,47 @@
 # from pickle import APPEND as app
 # from urllib import request
 # import base64
+import sys
+
+# setting path
+sys.path.append('../DCSC-Final-Project')
+
 from flask import Flask, request
+from QualtricsAPI.Survey import Responses, Credentials
+import pymongo as pymagno
+from bson.binary import Binary
+import pandas as pd
+import datetime
+import os
+# NOTE: consider renaming to DCSC_final_project or DCSCfinalproject so that you can import normally
+secrets = __import__("DCSC-Final-Project.secrets")
+from minio import Minio
+
 # import jsonpickle
 # import base64
-# import  redis
 # import os
-# from minio import Minio
 # import os,sys
 # import hashlib
 # import io
-#from QualtricsAPI.Survey import Responses
 
-# #instantiate redis
-# redisHost = os.getenv("REDIS_HOST") or "localhost"
-# redisPort = os.getenv("REDIS_PORT") or 6379
-#
-# redisClient = redis.StrictRedis(host=redisHost, port=redisPort, db=0)
-# #redisClient.flushdb()
-#
-# #instantiate minio
-# minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
-# minioUser = os.getenv("MINIO_USER") or "rootuser"
-# minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
-# print("{} {} {}".format(minioHost, minioUser, minioPasswd))
-#
-# minioclient = Minio(minioHost,
-#                secure=False,
-#                access_key=minioUser,
-#                secret_key=minioPasswd)
-#
-# bucketname="queue"
-# if not minioclient.bucket_exists(bucketname):
-#     print(f"Create bucket {bucketname}")
-#     minioclient.make_bucket(bucketname)
+#all the creds in the git ignore
+
+global MONGO_CLIENT
+
+
+def connect_to_mongo():
+    global MONGO_CLIENT
+    MONGO_CLIENT = pymagno.MongoClient(secrets.MONGO_ATLAS_CONNECTION_URL, serverSelectionTimeoutMS=5000)
+    try:
+        print(MONGO_CLIENT.server_info())
+    except Exception as e:
+        print('Unable to connect!')
+        print(e)
+
+
+def get_mongo_collection():
+    return MONGO_CLIENT[secrets.MONGO_DB_NAME][secrets.MONGO_DB_COLLECTION]
+
 
 app = Flask(__name__)
 
@@ -47,7 +55,7 @@ app = Flask(__name__)
 #     token = request.args.get("token")
 #     data_center = request.args.get("data_center")
 #     directory_id = request.args.get("directory_id")
-#     c.qualtrics_api_credentials(token='gEjVMlVLOJR33JJNnTQLOQTJmwBr0OOGY8mseOum',data_center='ca1',directory_id='POOL_3EQ4ctAv5niLXRT')
+#     c.qualtrics_api_credentials(token='token',data_center='dc',directory_id='d_id')
 #    #request to qualtrics api
 #
 #     #Create an instance
@@ -105,35 +113,144 @@ app = Flask(__name__)
 #             'songhash': songhash}
 #     return response
 
-valid_args = ['datacenter', 'token', 'directory_id']
-@app.route('/getSurvey', methods=['GET'])
+get_survey_args = ['datacenter', 'token', 'directory_id']
+
+
+@app.route('/getSurveys', methods=['GET'])
 def getSurvey():
     # Validate.
     args = request.args
     args = args.to_dict()
-    for k,v in args.items():
-        if k not in valid_args:
+    for k, v in args.items():
+        if k not in get_survey_args:
             return 'Bad Argument', 400
     if len(args) != 3:
         return 'Missing Argument', 400
 
-    # Perform Task
-    try:
-        perform_stuff
-    except QualtricException e: #check if qualtrics THROWS error codes; may need to parse response if not clear
-        if e.code == 403:
-            return "Qualtrics threw this error! {}".format(e)
-    except otherex:
-    except Exception e:
-    finally:
+    # create_qualtrics_credentials(args)
+    # survey_df = r.get(survey=survey_id)
+
+    # # Perform Task
+    # try:
+    #     perform_stuff
+    # except QualtricException e: #check if qualtrics THROWS error codes; may need to parse response if not clear
+    #     if e.code == 403:
+    #         return "Qualtrics threw this error! {}".format(e)
+    # except otherex:
+    # except Exception e:
+    # finally:
 
     # return to user
     return args
 
-print('hello')
-#start flask
-app.run(host="0.0.0.0",port=5000)
-print('goodbye')
 
-#to test:
-#"localhost:5000/getSurvey?datacenter=ca1&token=123&directory_id=myid"
+clean_survey_args = ['datacenter', 'token', 'directory_id', 'cleaning_methods']
+cleaning_method_options = ['no_nan', 'no_test', 'tidy_demo', 'simple_summary']
+
+@app.route('/cleanSurvey/<string:survey_id>', methods=['POST'])
+def cleanSurvey(survey_id):
+    # Validate.
+    args = request.args
+    args = args.to_dict()
+    for k, v in args.items():
+        if k not in clean_survey_args:
+            return 'Bad Argument', 400
+    if len(args) != 4:
+        return 'Missing Argument', 400
+    cleaning_methods = args.get('cleaning_methods').split(',')
+    for method in cleaning_methods:
+        if method not in cleaning_method_options:
+            return 'Valid options for cleaning methods are {}'.format(cleaning_method_options)
+
+    create_qualtrics_credentials(args)
+    r = Responses()
+    survey_df = r.get_survey_responses(survey=survey_id)
+
+    # Apply cleaning methods.
+    for method in cleaning_methods:
+        if method == 'no_nan':
+            # Emily's code
+            continue
+        elif method == 'no_test':
+            # Emily's code
+            continue
+        elif method == 'tidy_demo':
+            survey_df = tidy_demo(survey_df)
+        elif method == 'simple_summary':
+            return simple_summary(survey_df)
+
+    # Send data to Mongo.
+    survey_csv = survey_df.to_csv()
+    mongo_entry = {'survey_id': survey_id, 'survey_csv': Binary(survey_csv)}
+    mongo_result = get_mongo_collection().insert_one(mongo_entry)
+
+    # Return mongo result to user.
+    if not mongo_result.acknowledged:
+        return 'Mongo error', 400
+    else:
+        return {"mongo_document_id": mongo_result.inserted_id}
+
+@app.route('/retrieveMDBSurvey/<string:mongo_document_id>', methods=['GET'])
+def retrieveMDBSurvey(mongo_document_id):
+    # Validate.
+    args = request.args
+    args = args.to_dict()
+    for k, v in args.items():
+        if k not in clean_survey_args:
+            return 'Bad Argument', 400
+    if len(args) != 1:
+        return 'Missing Argument', 400
+
+    # Set up before connecting
+    today = datetime.today()
+    today = today.strftime("%m-%d-%Y")
+    _, _, instance_col = set_db()
+    # make an API call to the MongoDB server
+    mongo_docs = instance_col.find()
+
+    # Convert the mongo docs to a DataFrame
+    docs = pd.DataFrame(mongo_docs)
+    # Discard the Mongo ID for the documents
+    docs.pop("_id")
+
+    # compute the output file directory and name
+    output_dir = os.path.join('..', '..', 'output_files', 'aws_instance_list', 'csv', '')
+    output_file = os.path.join(output_dir, 'aws-instance-master-list-' + today +'.csv')
+
+    # export MongoDB documents to a CSV file, leaving out the row "labels" (row numbers)
+    docs.to_csv(output_file, ",", index=False) # CSV delimited by commas
+
+
+##HELPERS
+# Make qualtrics credentials using the user-provided token, datacenter and directory ID
+def create_qualtrics_credentials(args):
+    c = Credentials()
+    c.qualtrics_api_credentials(token=args.get('token'), data_center=args.get('datacenter'),
+                                directory_id=args.get('directory_id'))
+
+#remove all metadata columns except survey duration, recorded date and recorded id
+def tidy_demo(survey_df):
+    survey_df = survey_df.drop(survey_df.columns[[0:5, 7, 10:17]],axis = 1)
+    return survey_df
+
+#retuns date range, number of rows (responses), any with finish code 0, avg duration
+def simple_summary(survey_df):
+    daterange_first = survey_df['RecordedDate'].iloc[0]  # first element
+    daterange_last = survey_df['RecordedDate'].iloc[-1]  # last element
+    response_n = len(survey_df.index)
+    unfinished_n = (survey_df["Finished"] == 0).sum()
+    avgduration = pd.mean(survey_df["Duration (in seconds)"])
+    print("Date range:", daterange_first, ":", daterange_last,
+          "Number of Responses:", response_n,
+          "Number of Unfinished Responses (Finished = F)", unfinished_n,
+          "Average Response Duration:", avgduration)
+
+
+print('Starting server...')
+connect_to_mongo()
+get_mongo_collection()
+# start flask
+app.run(host="0.0.0.0", port=5000)
+
+# to test:
+# "localhost:5000/getSurvey?datacenter=ca1&token=123&directory_id=myid"
